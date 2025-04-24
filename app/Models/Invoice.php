78857d5fe3e3ee6f1220d66106a445d2c8498c38
Paul <fgocsgo@gmail.com>
2025-04-24 17:39:32 +0200

    feat: add phpdoc properties

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,9 +4,23 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property bool $show_header
+ * @property bool $show_installation_row
+ * @property bool $show_account_number
+ * @property bool $show_qr_payment
+ * @property float $total
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Collection<int, InvoiceItem> $items
+ */
 final class Invoice extends Model
 {
     protected $casts = [
--- app/Models/InvoiceItem.php
+++ app/Models/InvoiceItem.php
@@ -4,9 +4,23 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $quantity
+ * @property string $unit
+ * @property float $unit_price
+ * @property float $installation_price
+ * @property float $total
+ * @property int $invoice_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Invoice $invoice
+ */
 final class InvoiceItem extends Model
 {
     /** @return BelongsTo<Invoice, covariant $this> */
